06ade696022ef72a87a00b7bd71443e6c43cefeb
dirkarnez <smalldirkalex@gmail.com>
2018-11-09 17:05:48 +0800

    - init

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+
+module.exports = {
+   mode: 'development',
+   context: path.join(__dirname, './src'),
+   entry: [
+     './index.ts'
+   ],
+   module: {
+    rules: [
+      {
+         test: /\.tsx?$/,
+         exclude: /node_modules/,
+         use: [
+           isProduction && {
+             loader: 'babel-loader',
+             options: { 
+               plugins: [
+                 "react-hot-loader/babel", 
+                 "syntax-dynamic-import"
+               ] 
+             }
+           },  
+           'ts-loader'
+         ].filter(Boolean)
+       }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      publicPath: '/'
+   },
+   target: 'node',
+   externals: {
+      uws: "uws"
+   }
+};
